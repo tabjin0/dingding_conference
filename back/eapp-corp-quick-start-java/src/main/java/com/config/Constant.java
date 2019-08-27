31625e096aed4e318bf5a7a492683d3aa41abd69
jinboy <zjdefine@163.com>
2019-08-27 13:04:07 +0800

    后端环境

---
+++ back/eapp-corp-quick-start-java/src/main/java/com/config/Constant.java
@@ -0,0 +1,15 @@
+package com.config;
+
+/**
+ * 项目中的常量定义类
+ */
+public class Constant {
+    /**
+     * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppKey
+     */
+    public static final String APP_KEY = "dinguujnrqcegpsnnqtn";
+    /**
+     * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppSecret
+     */
+    public static final String APP_SECRET="zz0jOR1EAXhUkZPXYoI-d-QQs_DOJh1Pe3Dbihw1XocRDzzN0epMnFFnEKoCODsp";
+}
