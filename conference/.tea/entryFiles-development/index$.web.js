fb7a01a67ac6390ba1e779238bffa6b0c263f392
jinboy <zjdefine@163.com>
2019-08-28 23:03:12 +0800

    tarbar ok

--- conference/.tea/entryFiles-development/index$.web.js
+++
@@ -1,9 +0,0 @@
-require('@alipay/appx-compiler/lib/sjsEnvInit');
-require('./config$');
-
-require('../../page/conferenceManagement/addConference/addConference');
-require('../../page/index/index');
-require('../../page/conferenceManagement/conferenceManager/conferenceManager');
-require('../../page/statisticalReport/statisticalReport/statisticalReport');
-require('../../page/issueDeclaration/issueDeclaration/issueDeclaration');
-require('../../page/meetingAgenda/meetingAgenda/meetingAgenda');
